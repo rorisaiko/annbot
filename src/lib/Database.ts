5b0b922f628810e3f1078c000fde93d3d32b9347
Rorisaiko <rorisaiko@pm.me>
2022-02-28 00:20:39 +0900

    Added 'dbSelectArray' method in Database class v1.11.2

--- src/lib/Database.ts
+++ src/lib/Database.ts
@@ -15,6 +15,11 @@ export class Database {
 		return rows;
 	}
 
+	async dbSelectArray(sql: string, data: any): Promise<RowDataPacket[][]> {
+		let rows = (await this.con.query<RowDataPacket[][]>({sql: sql, rowsAsArray: true}, data))[0];
+		return rows;
+	}
+
 	async dbInsDel(sql: string, data: any): Promise<OkPacket> {
 		let result = (await this.con.query<OkPacket>(sql, data))[0];
 		return result;
